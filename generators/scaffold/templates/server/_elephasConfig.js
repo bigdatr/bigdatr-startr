0b45135e2e7ad9b184a0317d83c6d208f52b477e
Allan Hortle <allanhortle@gmail.com>
2016-06-18 20:53:16 +1000

    Make env.PORT the first fallback

--- generators/scaffold/templates/server/_elephasConfig.js
+++ generators/scaffold/templates/server/_elephasConfig.js
@@ -6,7 +6,7 @@ var elephas_config = {
     static_root_path: __dirname.replace('server', 'public') + '/',
     httpsOnly: false,
     server: {
-        port: process.env['<%= nameConstant %>_PORT'] || 3000,
+        port: process.env['<%= nameConstant %>_PORT'] || process.env.PORT || 3000,
     },
     csp: false
 };
