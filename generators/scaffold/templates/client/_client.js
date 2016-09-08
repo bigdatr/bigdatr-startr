422e38c8e37f69a6bcd84fb00ca6449eefe40523
Allan Hortle <allanhortle@gmail.com>
2016-09-08 20:50:48 +1000

    remove redux devtools in favour of chrome plugin

--- generators/scaffold/templates/client/_client.js
+++ generators/scaffold/templates/client/_client.js
@@ -1,30 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import {Router} from 'react-router';
-import { browserHistory } from 'react-router';
-import { Provider } from 'react-redux';
-
+import {browserHistory} from 'react-router';
+import {Provider} from 'react-redux';
 import store from '<%= name %>/client/store';
 import routes from '<%= name %>/client/routes';
 import clientStyles from '<%= name %>/client/sass/styles.scss';
 
-var appElement = document.getElementById('<%= name %>');
+const appElement = document.getElementById('<%= name %>');
 
-function renderDevtools() {
-    if (process.env.NODE_ENV === 'development') {
-        var Devtools = require('<%= name %>/client/devtools');
-        return <Devtools store={store} />;
-    }
-}
-
-//
-// Render Town
-//
 ReactDOM.render((
     <div>
         <Provider store={store}>
             <Router history={browserHistory} routes={routes}/>
         </Provider>
-        {renderDevtools()}
     </div>
 ), appElement);
--- generators/scaffold/templates/client/_store.js
+++ generators/scaffold/templates/client/_store.js
@@ -1,6 +1,6 @@
 import thunk from 'redux-thunk';
 import reducers from '<%= name %>/client/reducers';
-import { compose, createStore, applyMiddleware } from 'redux';
+import {compose, createStore, applyMiddleware} from 'redux';
 
 // create middleware
 var middleware = applyMiddleware(
@@ -10,19 +10,10 @@ var middleware = applyMiddleware(
 // Construct the store creator
 var createApplicationStore;
 if(process.env.NODE_ENV === 'development') {
-	var Devtools = require('<%= name %>/client/devtools');
-    var {persistState} = require('redux-devtools');
-
     createApplicationStore = compose(
         middleware,
-		Devtools.instrument(),
-        persistState(getDebugSessionKey())
+        window.devToolsExtension ? window.devToolsExtension() : f => f
     )(createStore);
-
-    function getDebugSessionKey(){
-        const matches = window.location.href.match(/[?&]debug_session=([^&]+)\b/);
-		return (matches && matches.length > 0)? matches[1] : null;
-    }
 } else {
     createApplicationStore = middleware(createStore);
 }
