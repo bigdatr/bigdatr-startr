48ac063bd2000d36f57e2b2c1efcbd427590a7be
Allan Hortle <allan.hortle@blueflag.com.au>
2016-10-26 11:31:23 +1100

    remove unused apple-touch-icon ref

--- generators/scaffold/templates/server/_index.static.jsx
+++ generators/scaffold/templates/server/_index.static.jsx
@@ -28,7 +28,6 @@ export default (props) => {
                 <title><%= name %></title>
                 <meta name="description" content=""/>
                 <meta name="viewport" content="width=device-width, initial-scale=1"/>
-                <link rel="apple-touch-icon" href="apple-touch-icon.png"/>
                 {renderStyles()}
             </head>
             <body>
