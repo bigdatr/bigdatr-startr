e42d16d63d254982756afb5783f20d1faad8865e
Dan Coates <dan.coates@blueflag.com.au>
2016-12-23 15:47:52 +1100

    Updates so that deploy through circle ci works

--- generators/client/constants/prompts.js
+++ generators/client/constants/prompts.js
@@ -5,67 +5,78 @@ module.exports = function(props) {
             type: 'input',
             name: 'name',
             message: 'What is your project name?',
-            default: path.parse(process.cwd()).name // Default to current folder name
+            default: path.parse(process.cwd()).name, // Default to current folder name
+            store: true
         },
         {
             type: 'input',
             name: 'description',
             message: 'Enter a short project description:',
-            default: ''
+            default: '',
+            store: true
         },
         {
             type: 'input',
             name: 'version',
             message: 'What semver version should the project start on?',
-            default: '0.0.0'
+            default: '0.0.0',
+            store: true
         },
         {
             type: 'input',
             name: 'license',
             message: 'What license is the project distributed under?',
-            default: 'UNLICENSED'
+            default: 'UNLICENSED',
+            store: true
         },
         {
             type: 'confirm',
             name: 'private',
             message: 'Is this a private project?',
-            default: true
+            default: true,
+            store: true
         },
         {
             type: 'input',
             name: 'author',
             message: 'Who/what is the author of this project?',
-            default: ''
+            default: '',
+            store: true
         },
         {
             type: 'input',
             name: 'username',
             message: 'What is your github user/organisation name?',
-            store: ''
+            store: '',
+            store: true
         },
         {
             type: 'input',
             name: 'port',
             message: 'What port would you like the development server to run on?',
-            default: 3000
+            default: 3000,
+            store: true
         },
         {
             type: 'confirm',
             name: 'cognito',
             message: 'Do you want cognito? (note that prerendering is not compatible with cognito)',
-            default: true
+            default: true,
+            store: true
         },
         {
             type: 'confirm',
             name: 'prerender',
             message: 'Do you want prerendering? (note that prerendering is not compatible with cognito)',
-            default: false
+            default: false,
+            store: true
         },
         {
             type: 'confirm',
             name: 'segment',
             message: 'Would you like segment tracking?',
-            default: true
+            default: true,
+            store: true
         },
         {
             type: 'input',
@@ -74,7 +85,8 @@ module.exports = function(props) {
             default: '',
             when: function(answers) {
                 return answers.segment; // Only ask this if user wanted segment
-            }
+            },
+            store: true
         }
 
     ];
