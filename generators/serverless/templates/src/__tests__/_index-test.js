1f4166a195b1614e066d5735de2e1bb91ad2e0b3
Dan Coates <dan.coates@blueflag.com.au>
2017-01-10 09:45:44 +1100

    Updates to graphql request processing and small style changes

--- generators/serverless/templates/src/__tests__/_index-test.js
+++ generators/serverless/templates/src/__tests__/_index-test.js
@@ -15,7 +15,7 @@ const LAMBDA_CONTEXT = {
     callbackWaitsForEmptyEventLoop: false
 };
 
-test('graphqlApi should execute the graphql query', (t: AssertContext): void => {
+test('graphqlApi should execute the graphql query', (t: AssertContext): Promise<void> => {
     const httpEvent = {
         body: JSON.stringify({
             query: 'query { viewer { username } }',
@@ -31,13 +31,12 @@ test('graphqlApi should execute the graphql query', (t: AssertContext): void => 
             return t.fail();
         }
 
-        t.is(TOKEN_PAYLOAD.username, result.body.data.viewer.username);
+        t.is(TOKEN_PAYLOAD.username, JSON.parse(result.body).data.viewer.username);
     };
-
     return graphqlApi(httpEvent, LAMBDA_CONTEXT, callback);
 });
 
-test('graphqlApi should execute the graphql query even if no variables are provided', (t: AssertContext): void => {
+test('graphqlApi should execute the graphql query even if no variables are provided', (t: AssertContext): Promise<void> => {
     const httpEvent = {
         body: JSON.stringify({
             query: 'query { viewer { username } }'
@@ -52,13 +51,64 @@ test('graphqlApi should execute the graphql query even if no variables are provi
             return t.fail();
         }
 
-        t.is(TOKEN_PAYLOAD.username, result.body.data.viewer.username);
+        t.is(TOKEN_PAYLOAD.username, JSON.parse(result.body).data.viewer.username);
     };
 
     return graphqlApi(httpEvent, LAMBDA_CONTEXT, callback);
 });
 
-test('graphqlApi should complain if no query is provided', (t: AssertContext): void => {
+test('graphqlApi should stringify body', (t: AssertContext): Promise<void> => {
+    const httpEvent = {
+        body: '{}',
+        headers: {
+            Authorization: JWT
+        }
+    };
+
+    const callback = (err: Error, result: Object) => {
+        t.is(typeof result.body, 'string');
+    };
+
+    return graphqlApi(httpEvent, LAMBDA_CONTEXT, callback);
+});
+
+
+test('graphqlApi handles malformed or missing auth header', (t: AssertContext): Promise<void> => {
+    const httpEvent = {
+        body: '{}',
+        headers: {
+            Authorization: 'NOTAJWT asfdlkasdlasdfkjhasi8yw3498y2sfd'
+        }
+    };
+
+    const callback = (err: Error, result: Object) => {
+        t.true(result.statusCode === 403);
+    };
+
+    return graphqlApi(httpEvent, LAMBDA_CONTEXT, callback);
+});
+
+
+test('graphqlApi should complain if no data is able to be returned due to a invalid query', (t: AssertContext): Promise<void> => {
+    const httpEvent = {
+        body: JSON.stringify({
+            query: 'query { notarealquery { somethingwrong } }'
+        }),
+        headers: {
+            Authorization: JWT
+        }
+    };
+
+    const callback = (err: Error, result: Object) => {
+        t.true(result.statusCode >= 400);
+        t.true(JSON.parse(result.body).errors.length > 0);
+    };
+
+    return graphqlApi(httpEvent, LAMBDA_CONTEXT, callback);
+});
+
+
+test('graphqlApi should complain if no query is provided', (t: AssertContext): Promise<void> => {
     const httpEvent = {
         body: '{}',
         headers: {
@@ -69,13 +119,13 @@ test('graphqlApi should complain if no query is provided', (t: AssertContext): v
     const callback = (err: Error, result: Object) => {
         t.ifError(err);
         t.true(result.statusCode >= 400);
-        t.true(result.body.errors.length > 0);
+        t.true(JSON.parse(result.body).errors.length > 0);
     };
 
     return graphqlApi(httpEvent, LAMBDA_CONTEXT, callback);
 });
 
-test('graphqlApi should catch any graphql errors', (t: AssertContext): void => {
+test('graphqlApi should catch any graphql errors', (t: AssertContext): Promise<void> => {
     const httpEvent = {
         body: JSON.stringify({
             query: 'query { viewer { username } }'
--- generators/serverless/templates/src/graphql/types/Viewer/_dataloaders.js
+++ generators/serverless/templates/src/graphql/types/Viewer/_dataloaders.js
@@ -1,22 +1,22 @@
 // @flow
 
-import createLoader from '<%= name %>/shared/utils/createLoader';
-import * as loaders from '<%= name %>/graphql/loaders';
+import createLoader from 'with-graphql/shared/utils/createLoader';
+import * as loaders from 'with-graphql/graphql/loaders';
 
 // Convert each function into a DataLoader
 export default function dataLoaders(): Object {
     return Object.keys(loaders)
-            .map((moduleName: string): Object => ({
-                [moduleName]: initLoaders(loaders[moduleName])
-            }))
-            .reduce((state: Object, val: Object): Object => Object.assign({}, state, val), {});
+        .map((moduleName: string): Object => ({
+            [moduleName]: initLoaders(loaders[moduleName])
+        }))
+        .reduce((state: Object, val: Object): Object => Object.assign({}, state, val), {});
 }
 
 
 function initLoaders(loaderModule: Object): Object {
     return Object.keys(loaderModule)
-                .map((loaderName: string): Object => ({
-                    [loaderName]: createLoader(loaderModule[loaderName])
-                }))
-                .reduce((state: Object, val: Object): Object => Object.assign({}, state, val), {});
+        .map((loaderName: string): Object => ({
+            [loaderName]: createLoader(loaderModule[loaderName])
+        }))
+        .reduce((state: Object, val: Object): Object => Object.assign({}, state, val), {});
 }
