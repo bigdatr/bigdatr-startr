252c2ae81004c10318f50d5c8cab7d839fb8752d
Dan Coates <dan.coates@blueflag.com.au>
2017-01-12 10:55:03 +1100

    Fix style loader relative paths issue when using webpack-dev-server

--- generators/client/templates/src/_client.js
+++ generators/client/templates/src/_client.js
@@ -1,5 +1,12 @@
 /* @flow */
 
+// A fix for style-loader issue #96, #55, #124
+if(process.env.NODE_ENV === 'development') {
+    /* eslint-disable */
+    __webpack_public_path__ = window.location.protocol + "//" + window.location.host + "/";
+    /* eslint-enable */
+}
+
 import 'whatwg-fetch';
 import React from 'react';
 import ReactDOM from 'react-dom';
@@ -9,7 +16,9 @@ import {Provider} from 'react-redux';
 import store from '<%= name %>/store';
 import routes from '<%= name %>/routes';
 <% if(segment) { %>import tracking from '<%= name %>/tracking';<% } %>
-import '<%= name %>/sass/styles.scss';
+
+// Needs to be required rather than imported for above fix to work
+require('bigdatr-wowo-client/sass/styles.scss');
 
 const appElement = document.getElementById('<%= name %>');
 
